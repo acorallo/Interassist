eac43173d3a3b20214560d06954e87e9ab3eab74
Andres Corallo <acorallo@gmail.com>
2017-05-17 18:02:28 -0300

    Interface Version Inicial

---
+++ InterAssist.Interface/Models/ResponseApi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace InterAssist.Interface.Models
+{
+    public class ResponseApi
+    {
+        public string toJson()
+        {
+            var json = new JavaScriptSerializer().Serialize(this);
+            return json;
+        }
+    }
+}
---
+++ InterAssist.Interface/Models/SendAssistanceResponseAPI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InterAssist.Interface.Models
+{
+    public class SendAssistanceResponseAPI : ResponseApi
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+    }
+}
